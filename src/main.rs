a5767d19259f8f2ef7a50ac4bd223b1e7e4a4686
Ash <ashtondsouza192@gmail.com>
2026-06-17 10:35:10 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
-        // let mut input = String::new();
-        // io::stdin().read_line(&mut input).unwrap();
-        // println!("You entered: {}", input.trim());
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        println!("{}: command not found", input.trim());
 }
