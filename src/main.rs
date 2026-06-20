ba8a3532024daee2c19b0c6cec93b7fb7c59402e
Ash <ashtondsouza192@gmail.com>
2026-06-20 13:37:07 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ fn parse_args(input: &str) -> Vec<String> {
     let mut args = Vec::new();
     let mut current = String::new();
     let mut in_single_quote = false;
-    let mut chars = input.chars().peekable();
+    let mut in_double_quote = false;
+    let mut chars = input.chars();
 
     while let Some(ch) = chars.next() {
         if in_single_quote {
@@ -19,8 +20,16 @@ fn parse_args(input: &str) -> Vec<String> {
             } else {
                 current.push(ch);
             }
+        } else if in_double_quote {
+            if ch == '"' {
+                in_double_quote = false;
+            } else {
+                current.push(ch);
+            }
         } else if ch == '\'' {
             in_single_quote = true;
+        } else if ch == '"' {
+            in_double_quote = true;
         } else if ch.is_whitespace() {
             if !current.is_empty() {
                 args.push(current.clone());
